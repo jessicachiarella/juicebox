ba5112cfcc091c42c31809f0e9590bdbb2361d1a
Tanner <tannerazm@yahoo.com>
2022-07-15 12:04:50 -0600

    completed part 1 of 1 and we were able to create three usernames and passwords for albert, sandra, and glamgal and console log them

--- db/seed.js
+++ db/seed.js
@@ -36,8 +36,10 @@ async function dropTables(){
             console.log("Starting to create users...");
 
             const albert = await createUser({ username: 'albert', password: 'bertie99'});
+            const sandra = await createUser({ username: 'sandra', password: '2sandy4me' });
+            const glamgal = await createUser({ username: 'glamgal', password: 'soglam' });
 
-            console.log(albert);
+            console.log(sandra)
 
             console.log("Finished creating users!");
         }catch(error){
